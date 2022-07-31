b5dfabf3c631f903830286d86d6c3378cec2ab3b
Rodrigo Eleuterio <roeleuss@gmail.com>
2022-07-30 22:30:39 -0300

    add script para instação do aplicativo para gerenciamento de partições, necessário a adição de disco virtuais

---
+++ ubuntu/22.04/gparted.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+####################################################
+# Aplicativo para configurar partições no sistema
+# https://joeferguson.me/adding-another-disk-to-wsl2/
+####################################################
+
+sudo apt install -y gparted
